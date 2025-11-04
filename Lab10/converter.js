aafed49d3c57a77c7fe93bfe0dad2997f3094355
V1LKA <adlet.askergeldi27@gmail.com>
2025-11-04 14:33:26 +0500

    Delete Lab10 directory

--- Lab10/converter.js
+++
@@ -1,8 +0,0 @@
-// converter.js
-export function usdToKzt(usd) {
-    return usd * 500; // пример курса — в отчёте укажи что это демонстрационный курс
-}
-
-export function kztToUsd(kzt) {
-    return kzt / 500;
-}
--- Lab10/formatter.js
+++
@@ -1,4 +0,0 @@
-// formatter.js
-export function format(label, value, currency) {
-    return `${label}: ${value.toFixed(2)} ${currency}`;
-}
